21adaa79ece725b9bf420ca326bb647e0cd0e0df
vividswan <vividswan@naver.com>
2024-03-29 23:11:27 +0900

    feat: gitkeep 삭제, fromEntity 메서드 생성

--- src/main/kotlin/com/vividswan/reentrantlock/controller/product/ProductDto.kt
+++ src/main/kotlin/com/vividswan/reentrantlock/controller/product/ProductDto.kt
@@ -1,3 +1,11 @@
 package com.vividswan.reentrantlock.controller.product
 
-class ProductDto(val id: Long, var stock: Int)
+import com.vividswan.reentrantlock.domain.product.Product
+
+class ProductDto(val id: Long, var stock: Int) {
+    companion object {
+        fun fromEntity(entity: Product): ProductDto {
+            return ProductDto(entity.id, entity.stock)
+        }
+    }
+}

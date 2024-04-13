74382fbff50a28d5e691f50a2e5d8da0c158ddb1
vividswan <vividswan@naver.com>
2024-04-14 01:04:41 +0900

    feat: dto 필드 타입 변경

--- src/main/kotlin/com/vividswan/reentrantlock/controller/product/ProductDto.kt
+++ src/main/kotlin/com/vividswan/reentrantlock/controller/product/ProductDto.kt
@@ -2,7 +2,7 @@ package com.vividswan.reentrantlock.controller.product
 
 import com.vividswan.reentrantlock.domain.product.Product
 
-class ProductDto(val id: Long, var stock: Int) {
+class ProductDto(val id: Long?, var stock: Int) {
     companion object {
         fun fromDomain(entity: Product): ProductDto {
             return ProductDto(entity.id, entity.stock)
